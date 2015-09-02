e2c3fdb0f7113c3fdf351260dc2556dd9ec152ca
Rémy Ferré <dev@remyferre.net>
2015-09-02 13:53:14 +0100

    Add a way to indent logs.

--- cxlogger.go
+++ cxlogger.go
@@ -3,9 +3,16 @@ package cxlogger
 // see http://godoc.org/github.com/inconshreveable/log15 for more info
 import (
 	"fmt"
+	"math/rand"
+	"strings"
+	"time"
 
 	log "gopkg.in/inconshreveable/log15.v2"
 )
+
+const tabWidth = 2
+
+var contextIndentations = make(map[string]int)
 
 // func main() {
 // 	Initialize("STDOUT", "debug")
@@ -14,7 +21,8 @@ import (
 
 type Logger struct {
 	log.Logger
-	Level log.Lvl
+	Context string
+	Level   log.Lvl
 }
 
 const (
@@ -27,11 +35,33 @@ const (
 
 var Log *Logger
 
-func Initialize(logOut string, lvl interface{}) error {
+func NewWithContext(context string, logOut string, lvl interface{}) (*Logger, error) {
+	l := Logger{}
+	return &l, l.InitializeWithContext(context, logOut, lvl)
+}
+
+func New(logOut string, lvl interface{}) (*Logger, error) {
+	l := Logger{}
+	return &l, l.Initialize(logOut, lvl)
+}
+
+func (l *Logger) InitializeWithContext(context string, logOut string, lvl interface{}) error {
+	l.Context = context
+	return l.Initialize(logOut, lvl)
+}
+
+func (l *Logger) Initialize(logOut string, lvl interface{}) error {
 	var (
 		level log.Lvl
 		err   error
 	)
+
+	if l.Context == "" {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		randInt := r.Int()
+		t := time.Now()
+		l.Context = fmt.Sprintf("%s#%d", t.Format(time.RFC3339), randInt)
+	}
 
 	if str, ok := lvl.(string); ok {
 		level, err = log.LvlFromString(str)
@@ -41,109 +71,55 @@ func Initialize(logOut string, lvl interface{}) error {
 	} else {
 		level = lvl.(log.Lvl)
 	}
-	Log = &Logger{log.New(), level}
+	l.Logger = log.New(log.Ctx{"context": l.Context})
+	l.Level = level
 
 	if logOut == "STDOUT" {
 		normalHandler := log.LvlFilterHandler(level, log.StdoutHandler)
 		errorHandler := log.LvlFilterHandler(level, log.CallerStackHandler("%+v", log.StdoutHandler))
 		handler := ErrorMultiHandler(normalHandler, errorHandler)
-		Log.SetHandler(handler)
+		l.SetHandler(handler)
 	} else if logOut == "NONE" {
-		Log.SetHandler(log.DiscardHandler())
+		l.SetHandler(log.DiscardHandler())
 	} else {
 		fileHandler := log.Must.FileHandler(logOut, log.LogfmtFormat())
 		normalHandler := log.LvlFilterHandler(level, fileHandler)
 		errorHandler := log.LvlFilterHandler(level, log.CallerStackHandler("%+v", fileHandler))
 		handler := ErrorMultiHandler(normalHandler, errorHandler)
-		Log.SetHandler(handler)
+		l.SetHandler(handler)
 	}
 
 	return nil
 }
 
 func (l *Logger) Debug(v ...interface{}) {
-	if interfaces, ok := v[0].([]interface{}); ok {
-		v = interfaces
-	}
-	err, ok := v[0].(error)
-	if ok {
-		l.Logger.Debug(err.Error(), "err", err)
-	} else {
-		msg := v[0].(string)
-		if len(v) > 1 {
-			l.Logger.Debug(msg, v[1:])
-		} else {
-			l.Logger.Debug(msg)
-		}
-	}
+	l.output(func(l log.Logger, msg string, v ...interface{}) {
+		l.Debug(msg, v...)
+	}, v)
 }
 
 func (l *Logger) Info(v ...interface{}) {
-	if interfaces, ok := v[0].([]interface{}); ok {
-		v = interfaces
-	}
-	err, ok := v[0].(error)
-	if ok {
-		l.Logger.Info(err.Error(), "err", err)
-	} else {
-		msg := v[0].(string)
-		if len(v) > 1 {
-			l.Logger.Info(msg, v[1:])
-		} else {
-			l.Logger.Info(msg)
-		}
-	}
+	l.output(func(l log.Logger, msg string, v ...interface{}) {
+		l.Info(msg, v...)
+	}, v)
 }
 
 func (l *Logger) Warn(v ...interface{}) {
-	if interfaces, ok := v[0].([]interface{}); ok {
-		v = interfaces
-	}
-	err, ok := v[0].(error)
-	if ok {
-		l.Logger.Warn(err.Error(), "err", err)
-	} else {
-		msg := v[0].(string)
-		if len(v) > 1 {
-			l.Logger.Warn(msg, v[1:])
-		} else {
-			l.Logger.Warn(msg)
-		}
-	}
+	l.output(func(l log.Logger, msg string, v ...interface{}) {
+		l.Warn(msg, v...)
+	}, v)
 }
 
 func (l *Logger) Error(v ...interface{}) {
-	if interfaces, ok := v[0].([]interface{}); ok {
-		v = interfaces
-	}
-	err, ok := v[0].(error)
-	if ok {
-		l.Logger.Error(err.Error(), "err", err)
-	} else {
-		msg := v[0].(string)
-		if len(v) > 1 {
-			l.Logger.Error(msg, v[1:])
-		} else {
-			l.Logger.Error(msg)
-		}
-	}
+	l.output(func(l log.Logger, msg string, v ...interface{}) {
+		l.Error(msg, v...)
+	}, v)
 }
 
 func (l *Logger) Crit(v ...interface{}) {
-	if interfaces, ok := v[0].([]interface{}); ok {
-		v = interfaces
-	}
-	err, ok := v[0].(error)
-	if ok {
-		l.Logger.Crit(err.Error(), "err", err)
-	} else {
-		msg := v[0].(string)
-		if len(v) > 1 {
-			l.Logger.Crit(msg, v[1:])
-		} else {
-			l.Logger.Crit(msg)
-		}
-	}
+	l.output(func(l log.Logger, msg string, v ...interface{}) {
+		l.Crit(msg, v...)
+	}, v)
 }
 
 func ErrorMultiHandler(normalHandler, errorHandler log.Handler) log.Handler {
@@ -169,6 +145,48 @@ func (l *Logger) Warnf(format string, v ...interface{})  { l.Warn(fmt.Sprintf(fo
 func (l *Logger) Errorf(format string, v ...interface{}) { l.Error(fmt.Sprintf(format, v...)) }
 func (l *Logger) Critf(format string, v ...interface{})  { l.Crit(fmt.Sprintf(format, v...)) }
 
+func (l *Logger) IncreaseIndentation() {
+	contextIndentations[l.Context] += 1
+}
+
+func (l *Logger) DecreaseIndentation() {
+	contextIndentations[l.Context] -= 1
+}
+
+func (l *Logger) output(logFunc func(l log.Logger, msg string, v ...interface{}), v ...interface{}) {
+	if interfaces, ok := v[0].([]interface{}); ok {
+		v = interfaces
+	}
+	err, ok := v[0].(error)
+	if ok {
+		logFunc(l.Logger, l.currentIndentation()+err.Error(), "err", err)
+	} else {
+		msg := v[0].(string)
+		if len(v) > 1 {
+			logFunc(l.Logger, l.currentIndentation()+msg, v[1:])
+		} else {
+			logFunc(l.Logger, l.currentIndentation()+msg)
+		}
+	}
+}
+
+func (l *Logger) currentIndentation() string {
+	return Indentation(contextIndentations[l.Context])
+}
+
+func Indentation(tabs int) string {
+	return strings.Repeat(" ", tabWidth*tabs)
+}
+
+func Initialize(logOut string, lvl interface{}) error {
+	if Log == nil {
+		Log = &Logger{
+			Logger: log.New(),
+		}
+	}
+	return Log.InitializeWithContext("main", logOut, lvl)
+}
+
 func Debug(v ...interface{}) { Log.Debug(v) }
 func Info(v ...interface{})  { Log.Info(v) }
 func Warn(v ...interface{})  { Log.Warn(v) }
@@ -180,3 +198,6 @@ func Infof(format string, v ...interface{})  { Log.Infof(format, v) }
 func Warnf(format string, v ...interface{})  { Log.Warnf(format, v) }
 func Errorf(format string, v ...interface{}) { Log.Errorf(format, v) }
 func Critf(format string, v ...interface{})  { Log.Critf(format, v) }
+
+func IncreaseIndentation() { Log.IncreaseIndentation() }
+func DecreaseIndentation() { Log.DecreaseIndentation() }
